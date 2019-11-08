afc807b47f45f6657af685f51422d24ce58bfb2c
kujirahand <web@kujirahand.com>
2019-11-09 07:51:45 +0800

    ch4 に追加

---
+++ ch4/classifier5.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from sklearn import datasets
+from sklearn import model_selection
+from sklearn import neural_network
+
+# irisデータセット
+iris = datasets.load_iris()
+
+# 訓練データとテストデータに分割
+train_data, test_data, train_target, test_target \
+	= model_selection.train_test_split(
+		iris.data, iris.target, 
+		test_size=0.3, random_state=1)
+
+# モデルを学習
+clf = neural_network.MLPClassifier(
+	hidden_layer_sizes=(5,5), 
+	max_iter=1000)
+clf.fit(train_data, train_target)
+
+# モデルの検証
+score = clf.score(test_data, test_target)
+print('score=' + str(score))
+
+# 予測
+predicted = clf.predict([[4.0, 2.0, 3.0, 1.0]])
+print('predict=' + str(predicted))
+
